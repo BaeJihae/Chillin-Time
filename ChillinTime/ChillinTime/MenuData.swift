fc6c1d980ed4056745cd5b316e2a41e8c62f31ff
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-04-01 16:14:29 +0900

    create Data

---
+++ ChillinTime/ChillinTime/MenuData.swift
@@ -0,0 +1,15 @@
+//
+//  MenuData.swift
+//  ChillinTime
+//
+//  Created by 배지해 on 4/1/24.
+//
+
+import Foundation
+
+struct MenuData{
+    var name: String
+    var id: Int
+    var price: Int
+    var category: String
+}
