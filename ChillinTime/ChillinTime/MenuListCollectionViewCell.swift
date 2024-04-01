e1763a89c2394cd5a4e8b80b4be30ad7188d0d53
BaeJihae <95227560+BaeJihae@users.noreply.github.com>
2024-04-01 19:52:48 +0900

    CollectionView UI구성 및 MenuData 추가

---
+++ ChillinTime/ChillinTime/MenuListCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MenuListCollectionViewCell.swift
+//  ChillinTime
+//
+//  Created by 배지해 on 4/1/24.
+//
+
+import UIKit
+
+class MenuListCollectionViewCell: UICollectionViewCell {
+    
+}
